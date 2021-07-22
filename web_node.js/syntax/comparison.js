bf456d2c4d82c3c78a85049c1573c1f2c8c13eb5
namjiseong <skawltjd2000@naver.com>
2021-07-22 11:26:18 +0900

    js문법과 콘솔 입력값 출력하기

---
+++ web_node.js/syntax/comparison.js
@@ -0,0 +1,8 @@
+console.log(1+1);
+//비교연산자
+console.log(1==1); //true
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1<2); //false
+console.log(1===1); //true
+console.log(1===2); //false
