88d7ca8e98f0f1eb88a146f9016b2a946cb4b992
namjiseong <skawltjd2000@naver.com>
2021-07-23 15:49:49 +0900

    글쓰기로 파일 생성하기

--- web_node.js/main.js
+++ web_node.js/main.js
@@ -115,9 +115,12 @@ var app = http.createServer(function (request, response) {
             var post = qs.parse(body);
             var title = post.title;
             var description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+                response.writeHead(302,{Location: `/?id=${title}`});
+                response.end();
+            })
         });
-        response.writeHead(200);
-        response.end("success");
+        
         
     }else{
         
