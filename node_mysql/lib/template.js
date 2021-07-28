304cd3cd85ab9903ecefa73c5ed6c235fffb1208
namjiseong <skawltjd2000@naver.com>
2021-07-28 19:00:42 +0900

    mysql 저자 관리-목록, 생성 구현

--- node_mysql/lib/template.js
+++ node_mysql/lib/template.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HTML:function(title, list, body, control){
+  HTML: function (title, list, body, control) {
     return `
     <!doctype html>
     <html>
@@ -9,36 +9,62 @@ module.exports = {
     </head>
     <body>
       <h1><a href="/">WEB</a></h1>
+      <a href="/author">author</a>
       ${list}
       ${control}
       ${body}
     </body>
     </html>
     `;
-  },list:function(topics){
-    var list = '<ul>';
+  },
+  list: function (topics) {
+    var list = "<ul>";
     var i = 0;
-    while(i < topics.length){
-      list = list + `<li><a href="/?id=${topics[i].id}">${topics[i].title}</a></li>`;
+    while (i < topics.length) {
+      list =
+        list + `<li><a href="/?id=${topics[i].id}">${topics[i].title}</a></li>`;
       i = i + 1;
     }
-    list = list+'</ul>';
+    list = list + "</ul>";
     return list;
-  }, authorSelect:function(authors, author_id){
-  var tag = '';
-            var i = 0;
-            while(i < authors.length){
-              var selected = '';
-              if(authors[i].id === author_id){
-                selected = 'selected';
-              }
-              tag = tag + `<option value="${authors[i].id}"${selected}>${authors[i].name}</option>`;
-              i++;
-            }
-  return `
+  },
+  authorSelect: function (authors, author_id) {
+    var tag = "";
+    var i = 0;
+    while (i < authors.length) {
+      var selected = "";
+      if (authors[i].id === author_id) {
+        selected = "selected";
+      }
+      tag =
+        tag +
+        `<option value="${authors[i].id}"${selected}>${authors[i].name}</option>`;
+      i++;
+    }
+    return `
   <select name="author">
   ${tag}
   </select> 
-  `
-}
-}
+  `;
+  },
+  authorTable: function (authors) {
+    var tag = `<tr id=contents>
+      <td>id</td>
+      <td>name</td>
+      <td>profile</td>
+      </tr>`;
+    var i = 0;
+    while (authors.length > i) {
+    tag =tag +
+    `<tr>
+      <td>${authors[i].id}</td>
+      <td>${authors[i].name}</td>
+      <td>${authors[i].profile}</td>
+      <td>create</td>
+      <td>delete</td>
+    </tr>`;
+    i++;
+    }
+    return tag;
+  }
+};
--- node_mysql/main.js
+++ node_mysql/main.js
@@ -4,32 +4,35 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var db = require('./lib/db.js');
 var topic = require('./lib/topic');
+var author = require('./lib/author');
 
-
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-
-      if(queryData.id === undefined){
-        topic.home(request, response);
-      } else {
-        topic.page(request, response);
-      }
-    } else if(pathname === '/create'){
-        topic.create(request, response);
-    } else if(pathname === '/create_process'){
-        topic.create_process(request, response);
-    } else if(pathname === '/update'){
-        topic.update(request, response);
-    } else if(pathname === '/update_process'){
-        topic.update_process(request, response);
-    } else if(pathname === '/delete_process'){
-        topic.delete_process(request, response);
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      topic.home(request, response);
     } else {
-      response.writeHead(404);
-      response.end('Not found');
+      topic.page(request, response);
     }
+  } else if (pathname === "/create") {
+    topic.create(request, response);
+  } else if (pathname === "/create_process") {
+    topic.create_process(request, response);
+  } else if (pathname === "/update") {
+    topic.update(request, response);
+  } else if (pathname === "/update_process") {
+    topic.update_process(request, response);
+  } else if (pathname === "/delete_process") {
+    topic.delete_process(request, response);
+  } else if(pathname === "/author"){
+    author.home(request, response);
+  } else if(pathname === "/author/create_process"){
+    author.create_pocess(request, response);
+  }else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
 });
 app.listen(5500);
