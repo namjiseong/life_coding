6fb44b522b4de78789ceba4bdd75cc75ec29469f
namjiseong <skawltjd2000@naver.com>
2021-07-28 16:57:44 +0900

    Apply .gitignore

--- node_mysql/lib/db.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'qsdrwe159',
-    database:'open'
-  });
-  db.connect();
-module.exports = db;
